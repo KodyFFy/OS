4e09e2f222bae8e66770820ca53c7d366b32cba4
KodyFFy <62069785+KodyFFy@users.noreply.github.com>
2022-04-14 11:43:21 +0300

    aa
    aa

---
+++ Практика 3/main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Os_lab_2
+{
+    class main
+    {
+
+
+
+        static void Main()
+        {
+
+        }
+    }
+}
